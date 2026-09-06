574e4bc7b581256a2d2a2f70d161a0781c885eaf
ThiagoDiniz777 <thiago.messias.ezequiel.diniz@gmail.com>
2026-09-06 00:43:46 -0300

    alternativas

---
+++ src/main/java/paradecision/boot/modulos/respostaTipoAlternativa/entity/RespostaTipoAternativa.java
@@ -0,0 +1,11 @@
+package paradecision.boot.modulos.respostaTipoAlternativa.entity;
+
+public class RespostaTipoAternativa {
+
+    private long codigoRespostaTipoAltenativa;
+    private  String pergunta ;
+    private  Integer peso ;
+
+
+
+}
